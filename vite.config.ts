7f6e6777bf3e120dd41cb694fa73c53f970b34f4
o.nikolaev <o.nikolaev@iq-adv.ru>
2024-08-28 18:49:28 +0500

    Deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({mode}) => ({
   plugins: [react()],
   resolve: {
     alias: {
       '@': '/src'
     }
-  }
-})
+  },
+  
+  base: mode === "production" ? "/dnd-flexbox/" : "/"
+}))
